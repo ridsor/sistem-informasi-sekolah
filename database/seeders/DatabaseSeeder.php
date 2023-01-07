092b0949812ceaddc0e8ac199dfdf26c21bb30a6
Ridsor <ridsorgamerz@gmail.com>
2023-01-08 00:13:29 +0700

    penambahan model dan factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Siswa;
+use App\Models\Jurusan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Siswa::factory(12)->create();
+        
+        Jurusan::create([
+            'nm_jurusan' => 'Manajemen Perkantoran',
+            'slug' => 'manajemen-perkantoran',
+        ]);
+        Jurusan::create([
+            'nm_jurusan' => 'Akutansi dan Keuangan',
+            'slug' => 'akutansi-dan-keuangan',
+        ]);
+        Jurusan::create([
+            'nm_jurusan' => 'Bisnis dan Pemasaran',
+            'slug' => 'bisnis-dan-pemasaran',
+        ]);
+        Jurusan::create([
+            'nm_jurusan' => 'Rekayasa Perangkat Lunak',
+            'slug' => 'rekayasa-perangkat-lunak',
+        ]);
     }
 }
