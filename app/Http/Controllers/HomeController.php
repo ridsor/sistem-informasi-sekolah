e4e20ae3083540594c71b2453fe037cccacce239
Ridsor <ridsorgamerz@gmail.com>
2023-01-16 19:50:28 +0700

    pembuatan halaman login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        return view('home.index');
+        return view('home.index', [
+            'title' => 'Home'
+        ]);
     }
 }
