c93fe82ac4dc53ac3cb6bcee47b11cfeda3a220a
Ridsor <ridsorgamerz@gmail.com>
2023-01-21 17:40:52 +0700

    buat sidebar dan header

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboard.layouts.main')
+
+@section('content')
+<h1>halo bro!</h1>  
+@endsection
---
+++ resources/views/dashboard/layouts/header.blade.php
@@ -0,0 +1,37 @@
+<div class="container-search container-fluid align-items-center">
+  <button type="button" class="d-md-none btn border-0 p-0">
+    <label for="checkbox-search" class="btn btn-danger">
+      <i class="bi bi-x-lg"></i>
+    </label>
+  </button>
+  <form class="w-100 mx-2" role="search">
+    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+  </form>
+  <button type="button" class="d-md-none btn border-0 p-0">
+    <label for="" class="btn btn-outline-secondary">
+      <i class="bi bi-three-dots-vertical"></i>
+    </label>
+  </button>
+</div>  
+<nav class="container-fluid gap-3 align-items-center d-grid" style="grid-template-columns: 1fr 2fr;">
+  <div>
+    <img src="/img/profile.png" alt="" width="35" height="35" class="d-inline-block align-text-top rounded-circle"/>
+    <span class="account-name fw-semibold fs-6 d-none d-md-inline-block">Administator</span>
+  </div>       
+  <div class="d-flex align-items-center justify-content-end"> 
+    <form class="w-100 me-3 d-none d-md-block" role="search">
+      <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+    </form>
+    <img src="/img/logo-rpl.png" alt="mdo" width="35" height="35" class="rounded-circle d-none d-md-block">
+    <button type="button" class="d-md-none btn p-0 border-0 me-2">
+      <label for="checkbox-search" class="btn btn-outline-secondary">
+        <i class="bi bi-search"></i>
+      </label>
+    </button>
+    <button type="button" class="d-md-none btn p-0 border-0">
+      <label for="checkbox-collapse" class="btn btn-outline-secondary">
+        <i class="bi bi-arrows-collapse"></i>
+      </label>
+    </button>
+  </div>
+</nav>
