3b6823c3823349591b42737332379d664952c4a9
Ridsor <ridsorgamerz@gmail.com>
2023-01-22 13:51:42 +0700

    upgrade sidebar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Siswa;
+use App\Models\Kelas;
+use App\Models\Jurusan;
 
 class DashboardController extends Controller
 {
     public function index() {
         return view('dashboard.index', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'jumlahSiswa' => Siswa::count(),
+            'jumlahKelas' => Kelas::count(),
+            'jumlahJurusan' => Jurusan::count(),
         ]);
     }
 }
--- resources/views/dashboard/layouts/aside.blade.php
+++ resources/views/dashboard/layouts/aside.blade.php
@@ -1,44 +1,26 @@
 <div class="flex-shrink-0 p-3 bg-white border-end responsive-sidebar bg-secondary-subtle">
   <ul class="list-unstyled ps-0">
     <li class="mb-1">
-      <a href="#" class="single-link rounded link-dark text-decoration-none d-inline-flex btn btn-toggle">Dashboard</a>
+      <a href="/dashboard" class="single-link rounded link-dark text-decoration-none d-inline-flex btn btn-toggle{{ Request::is('dashboard') ? ' active' : '' }}"><i class="bi bi-grid-fill me-2"></i>Dashboard</a>
     </li>
     <li class="mb-1">
-      <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#siswa-collapse" aria-expanded="false">Siswa</button>
-      <div class="collapse" id="siswa-collapse">
-        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-          <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Pendaftaran</a></li>
-          <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Updates</a></li>
-          <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Reports</a></li>
-        </ul>
-      </div>
+      <a href="/dashboard/siswa" class="single-link rounded link-dark text-decoration-none d-inline-flex btn btn-toggle"><i class="bi bi-people-fill me-2{{ Request::is('dashboard/siswa') ? ' active' : '' }}"></i>Siswa</a>
     </li>
     <li class="mb-1">
-      <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#kelas-collapse" aria-expanded="false">Kelas</button>
-      <div class="collapse" id="kelas-collapse">
-        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-          <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
-          <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Updates</a></li>
-          <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Reports</a></li>
-        </ul>
-      </div>
-      </li>
-      <li class="mb-1">
-        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#jurusan-collapse" aria-expanded="false">Jurusan</button>
-        <div class="collapse" id="jurusan-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
-            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Updates</a></li>
-            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Reports</a></li>
-          </ul>
-        </div>
-      </li>
-      <li class="border-top my-3"></li>
-      <li class="mb-1">
-        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-          Account
-        </button>
-        <div class="collapse" id="account-collapse">
+      <a href="/dashboard/kelas" class="single-link rounded link-dark text-decoration-none d-inline-flex btn btn-toggle"><i class="bi bi-house-door-fill me-2{{ Request::is('dashboard/kelas') ? ' active' : '' }}"></i>Kelas</a>
+    </li>
+    <li class="mb-1">
+      <a href="/dashboard/jurusan" class="single-link rounded link-dark text-decoration-none d-inline-flex btn btn-toggle"><i class="bi bi-file-earmark-fill me-2{{ Request::is('dashboard/jurusan') ? ' active' : '' }}"></i>Jurusan</a>
+    </li>
+    <li class="mb-1">
+      <a href="/dashboard/laporan" class="single-link rounded link-dark text-decoration-none d-inline-flex btn btn-toggle"><i class="bi bi-book-fill me-2{{ Request::is('dashboard/laporan') ? ' active' : '' }}"></i>Laporan</a>
+    </li>
+    <li class="border-top my-3"></li>
+    <li class="mb-1">
+      <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+        Account
+      </button>
+      <div class="collapse" id="account-collapse">
         <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
           <li><a href="/logout" class="link-dark d-inline-flex text-decoration-none rounded">Log out</a></li>
         </ul>
