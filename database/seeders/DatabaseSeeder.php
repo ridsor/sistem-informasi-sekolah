f5b7fc2ef770893cc818fe20c9be31f38ee651e3
RYAN SYUKUR <ridsorgamerz@gmail.com>
2023-04-05 11:42:26 +0900

    menghapus data table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,90 +21,91 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         \App\Models\User::factory()->create([
-            'name' => 'User',
-            'email' => 'user@example.com',
+            'name' => 'Admin',
+            'email' => 'ridsorgamerz@gmail.com',
+            'is_admin' => true,
         ]);
 
-        Siswa::factory(12)->create();
-        Nilai::factory(12)->create();
-        NilaiMapel::factory(12*16)->create();
+        // Siswa::factory(12)->create();
+        // Nilai::factory(12)->create();
+        // NilaiMapel::factory(12*16)->create();
         
-        Jurusan::create([
-            'nm_jurusan' => 'Manajemen Perkantoran',
-            'slug' => 'manajemen-perkantoran',
-        ]);
-        Jurusan::create([
-            'nm_jurusan' => 'Akutansi dan Keuangan',
-            'slug' => 'akutansi-dan-keuangan',
-        ]);
-        Jurusan::create([
-            'nm_jurusan' => 'Bisnis dan Pemasaran',
-            'slug' => 'bisnis-dan-pemasaran',
-        ]);
-        Jurusan::create([
-            'nm_jurusan' => 'Rekayasa Perangkat Lunak',
-            'slug' => 'rekayasa-perangkat-lunak',
-        ]);
+        // Jurusan::create([
+        //     'nm_jurusan' => 'Manajemen Perkantoran',
+        //     'slug' => 'manajemen-perkantoran',
+        // ]);
+        // Jurusan::create([
+        //     'nm_jurusan' => 'Akutansi dan Keuangan',
+        //     'slug' => 'akutansi-dan-keuangan',
+        // ]);
+        // Jurusan::create([
+        //     'nm_jurusan' => 'Bisnis dan Pemasaran',
+        //     'slug' => 'bisnis-dan-pemasaran',
+        // ]);
+        // Jurusan::create([
+        //     'nm_jurusan' => 'Rekayasa Perangkat Lunak',
+        //     'slug' => 'rekayasa-perangkat-lunak',
+        // ]);
 
-        Kelas::create([
-            'jurusan_id' => 4,
-            'nm_kelas' => 'XD RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 4,
-            'nm_kelas' => 'XID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 4,
-            'nm_kelas' => 'XIID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 3,
-            'nm_kelas' => 'XIID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 2,
-            'nm_kelas' => 'XIID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 1,
-            'nm_kelas' => 'XIID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 3,
-            'nm_kelas' => 'XID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 3,
-            'nm_kelas' => 'XD RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 2,
-            'nm_kelas' => 'XID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 2,
-            'nm_kelas' => 'XD RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 1,
-            'nm_kelas' => 'XID RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 1,
-            'nm_kelas' => 'XD RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 3,
-            'nm_kelas' => 'XIIB RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 3,
-            'nm_kelas' => 'XIB RPL',
-        ]);
-        Kelas::create([
-            'jurusan_id' => 3,
-            'nm_kelas' => 'XB RPL',
-        ]);
+        // Kelas::create([
+        //     'jurusan_id' => 4,
+        //     'nm_kelas' => 'XD RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 4,
+        //     'nm_kelas' => 'XID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 4,
+        //     'nm_kelas' => 'XIID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 3,
+        //     'nm_kelas' => 'XIID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 2,
+        //     'nm_kelas' => 'XIID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 1,
+        //     'nm_kelas' => 'XIID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 3,
+        //     'nm_kelas' => 'XID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 3,
+        //     'nm_kelas' => 'XD RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 2,
+        //     'nm_kelas' => 'XID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 2,
+        //     'nm_kelas' => 'XD RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 1,
+        //     'nm_kelas' => 'XID RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 1,
+        //     'nm_kelas' => 'XD RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 3,
+        //     'nm_kelas' => 'XIIB RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 3,
+        //     'nm_kelas' => 'XIB RPL',
+        // ]);
+        // Kelas::create([
+        //     'jurusan_id' => 3,
+        //     'nm_kelas' => 'XB RPL',
+        // ]);
     }
 }
